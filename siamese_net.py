bee7f431494ff4e33df88bdac1c47a4d8c99adb1
dschappler <daniel.schappler@muenster.de>
2017-01-07 16:07:02 +0100

    Add files via upload

--- siamese_net.py
+++ siamese_net.py
@@ -10,7 +10,7 @@ import pandas as pd
 from keras.models import Model, Sequential
 from keras.layers import Input, Lambda, Flatten, Dense
 from keras import backend as K
-from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.applications.vgg16 import VGG16, preprocess_input
 import urllib, cStringIO
 from keras.preprocessing.image import img_to_array, load_img
 from keras.optimizers import RMSprop
@@ -54,25 +54,6 @@ def load_images(csv_file):
     return np.array(pairs), np.array(labels)
 
 
-def pred_and_label(model, URL): 
-    image = load_and_preprocess(URL)
-    pred = model.predict(image)
-    pred_label = (decode_predictions(pred)[0][0][1], decode_predictions(pred)[0][0][2])
-    return pred_label 
-    print('Predicted:', pred_label)
-
-
-def create_pairs():
-    '''Positive and negative pair creation.
-    '''
-    pairs = []
-    labels = []
-    z1, z2 = load_and_preprocess('http://www.pixempire.com/images/preview/black-circle-icon.jpg'), load_and_preprocess('http://www.masadaband.net/wp-content/uploads/2014/09/black-square.jpg')
-    pairs += [z1, z2]
-    labels += [0]
-    return np.array(pairs), np.array(labels)
-
-
 def create_base_network():
     '''Base network to be shared (eq. to feature extraction).
     '''
@@ -87,42 +68,60 @@ def compute_accuracy(predictions, labels):
     '''Compute classification accuracy with a fixed threshold on distances.
     '''
     return np.mean(labels == (predictions.ravel() > 0.5))
+    
+    
+def siam_cnn():
+    base_network = create_base_network()
+    input_a = Input(shape=(3,224,224,))
+    input_b = Input(shape=(3,224,224,))
+    # because we re-use the same instance `base_network`,
+    # the weights of the network
+    # will be shared across the two branches
+    processed_a = base_network(input_a)
+    processed_b = base_network(input_b)
+    distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+    model = Model(input=[input_a, input_b], output=distance)
+    #model.summary()
+    #model.get_config()
+    #model.get_weights()
+    return model
 
 
-# create training+test positive and negative pairs
-#OLD: tr_pairs, tr_y = create_pairs()
-tr_pairs, tr_y = load_images('val.csv')
+def train():
+    tr_pairs, tr_y = load_images('val.csv')
+    print("Images loaded.")
+    model = siam_cnn()
+    optimizer = RMSprop()
+    model.compile(loss=contrastive_loss, optimizer=optimizer)
+    print("Model compiled.")
+    model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+              batch_size=1,
+              nb_epoch=1)
+    return model, tr_pairs, tr_y
+    #TODO: optimal stopping, save weights, save model HISTORY
 
 
-# network definition
-base_network = create_base_network()
+def predict_and_evaluate():# compute final accuracy on training and test sets
+    model, tr_pairs, tr_y = train()
+    te_pairs, te_y = load_images('test.csv')
+    pred = model.predict([tr_pairs[4, 0], tr_pairs[4, 1]])
+    #evalu = model.evaluate([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y)
+    #testu = model.test_on_batch([tr_pairs[0], tr_pairs[1]], tr_y)
+    tr_acc = compute_accuracy(pred, tr_y)
+    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+    pred = model.predict([te_pairs[4, 0], te_pairs[4, 1]])
+    te_acc = compute_accuracy(pred, te_y)
+    print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
 
-input_a = Input(shape=(3,224,224,))
-input_b = Input(shape=(3,224,224,))
 
-# because we re-use the same instance `base_network`,
-# the weights of the network
-# will be shared across the two branches
-processed_a = base_network(input_a)
-processed_b = base_network(input_b)
+#TODO: visualize training
 
-distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
-model = Model(input=[input_a, input_b], output=distance)
-#model.summary()
-#model.get_config()
-#model.get_weights()
+#TODO: ROC & AUC calculation & visualization
 
-# train
-rms = RMSprop()
-model.compile(loss=contrastive_loss, optimizer=rms)
-model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
-          batch_size=1,
-          nb_epoch=1)
 
-# compute final accuracy on training and test sets
-pred = model.predict([tr_pairs[4, 0], tr_pairs[4, 1]])
-evalu = model.evaluate([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y)
-testu = model.test_on_batch([tr_pairs[0], tr_pairs[1]], tr_y)
-tr_acc = compute_accuracy(pred, tr_y)
-print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+#TODO: t-sne & visualization
+
+
+if __name__=="__main__":
+    train()
