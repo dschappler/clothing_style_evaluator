1546541beb916e74018e111e30ba387fdb93a817
dschappler <daniel.schappler@muenster.de>
2017-01-22 10:26:20 +0100

    Add files via upload

--- siamese_net.py
+++ siamese_net.py
@@ -5,17 +5,19 @@ for mode details).
 '''
 from __future__ import absolute_import
 from __future__ import print_function
+import urllib, cStringIO
+import progressbar as pb
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from keras.models import Model, Sequential
 from keras.layers import Input, Lambda, Flatten, Dense, Dropout
 from keras import backend as K
 from keras.applications.vgg16 import VGG16, preprocess_input
-import urllib, cStringIO
 from keras.preprocessing.image import img_to_array, load_img
-from keras.optimizers import RMSprop, SGD
-import progressbar as pb
-from sklearn.metrics import roc_auc_score
+from keras.optimizers import RMSprop#, SGD
+from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.cross_validation import train_test_split
 
 
 def euclidean_distance(vects):
@@ -73,6 +75,7 @@ def create_bottleneck_network():
     
     
 def create_base_network():
+    #TODO: add more regularization
     seq = Sequential()
     seq.add(Dense(256, activation='relu', input_dim=25088))
     seq.add(Dropout(0.5))
@@ -80,26 +83,43 @@ def create_base_network():
     return seq
     
     
-def save_bottleneck_features():
-    data = load_images('train12.csv')    
-    print('Images loaded.')    
-    model = create_bottleneck_network()
-    print('Model loaded.')
-    pairs = []
-    widgets = ['Progress: ', pb.Percentage(), ' ', pb.Bar(marker='0',left='[',right=']'),
-           ' ', pb.ETA(), ' ', pb.FileTransferSpeed(), ' ']
-    pbar = pb.ProgressBar(widgets=widgets, maxval=len(data))
-    pbar.start()
-    for i in range(len(data)):
-        pic_1 = model.predict(data['pic1'][i])[0]
-        pic_2 = model.predict(data['pic2'][i])[0]
-        pairs += [[pic_1, pic_2]]
-        pbar.update(i)
-    pbar.finish()
-    print('Finished. Saving features...')
-    np.save('bottleneck_pairs_12', np.asarray(pairs))
-    np.save('bottleneck_labels_12', np.asarray(data['score']))
-    print('Features saved.')
+def bottleneck_features(save=False):
+    if save:
+        data = load_images('train.csv')    
+        print('Images loaded.')    
+        model = create_bottleneck_network()
+        print('Model loaded.')
+        pairs = []
+        widgets = ['Progress: ', pb.Percentage(), ' ', pb.Bar(marker='0',left='[',right=']'),
+               ' ', pb.ETA(), ' ', pb.FileTransferSpeed(), ' ']
+        pbar = pb.ProgressBar(widgets=widgets, maxval=len(data))
+        pbar.start()
+        for i in range(len(data)):
+            pic_1 = model.predict(data['pic1'][i])[0]
+            pic_2 = model.predict(data['pic2'][i])[0]
+            pairs += [[pic_1, pic_2]]
+            pbar.update(i)
+        pbar.finish()
+        print('Finished. Saving features...')
+        np.save('bottleneck_pairs', np.asarray(pairs))
+        np.save('bottleneck_labels', np.asarray(data['score']))
+        print('Features saved.')
+    
+    print("Loading pairs..")
+    pairdata = np.load('bottleneck_pairs.npy')
+    pairdata = (pairdata - np.min(pairdata)) / (np.max(pairdata) - np.min(pairdata))
+    print("Loading labels..")
+    labeldata = np.load('bottleneck_labels.npy')
+    print("Data loaded.")
+    X_train, X_test, y_train, y_test = train_test_split(pairdata, labeldata, 
+                                                        test_size=.2,
+                                                        random_state=7,
+                                                        stratify=[1])
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, 
+                                                        test_size=.2,
+                                                        random_state=7,
+                                                        stratify=[1])
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 
 def compute_accuracy(predictions, labels):
@@ -119,50 +139,50 @@ def siam_cnn():
     processed_b = base_network(input_b)
     distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
     model = Model(input=[input_a, input_b], output=distance)
-    #model.summary()
-    #model.get_config()
-    #model.get_weights()
+    #TODO: model.summary(), model.get_config(), model.get_weights(), model.plot()
     return model
 
 
-def train():
-    print("Loading pairs..")
-    tr_pairs = np.load('bottleneck_pairs.npy')
-    tr_pairs = (tr_pairs - np.min(tr_pairs)) / (np.max(tr_pairs) - np.min(tr_pairs))
-    print("Loading labels..")
-    tr_y = np.load('bottleneck_labels.npy')
-    print("Data loaded.")
+def train_and_predict():
+    X_train, X_val, X_test, y_train, y_val, y_test = bottleneck_features()
     model = siam_cnn()
-    optimizer = SGD()
+    optimizer = RMSprop()
     model.compile(loss=contrastive_loss, optimizer=optimizer)
     print("Model compiled.")
-    model.fit([tr_pairs[:,0], tr_pairs[:,1]], tr_y,
-              validation_split=.3,
+    model.fit([X_train[:,0], X_train[:,1]], y_train,
+              validation_data = ([X_val[:,0], X_val[:,1]], y_val),
               batch_size=32,
               nb_epoch=5)
-    return model, tr_pairs, tr_y
-    #TODO: optimal stopping, save weights, save model HISTORY
+    #TODO: train acc, learning curve, optimal stopping, save weights, save model HISTORY
+    y_pred = model.predict([X_test[:,0], X_test[:,0]])
+    return y_test, y_pred
 
 
-def predict_and_evaluate():# compute final accuracy on training and test sets
-    model, tr_pairs, tr_y = train()
-    te_pairs, te_y = load_images('tst.csv')
-    pred = model.predict([tr_pairs[:,0,0], tr_pairs[:,1,0]])
-    #evalu = model.evaluate([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y)
-    #testu = model.test_on_batch([tr_pairs[0], tr_pairs[1]], tr_y)
-    tr_acc = compute_accuracy(pred, tr_y)
-    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
-    pred = model.predict([te_pairs[4, 0], te_pairs[4, 1]])
-    te_acc = compute_accuracy(pred, te_y)
+def evaluate():
+    # compute final accuracy on training and test sets
+    y_test, y_pred = train_and_predict()
+    te_acc = compute_accuracy(y_pred, y_test)
     print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
     
+    fpr, tpr, _ = roc_curve(y_test, y_pred, pos_label=1)
+    roc_auc = roc_auc_score(fpr, tpr)
+       
+    ##############################################################################
+    # Plot of a ROC curve for a specific class
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+    
 
-#TODO: visualize training
-#TODO: ROC & AUC calculation & visualization
 #TODO: t-sne & visualization
 
 
 if __name__=="__main__":
-    train()
-    
-#TODO: crossvalidation
+    evaluate()
